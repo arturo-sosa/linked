1a1a363e0c33c5549c3fb23659856011f95402f6
Arturo Sosa <61207851+arturo-sosa@users.noreply.github.com>
2022-11-25 08:12:50 -0600

    ♻ chore: astro config site on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import {defineConfig} from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({
-  site: `${process.env.DEPLOY_URL}`,
+const config = {
   integrations: [tailwind()]
-});
+};
+
+if (import.meta.env.PROD && process.env.DEPLOY_URL)
+  config.site = process.env.DEPLOY_URL;
+
+// https://astro.build/config
+export default defineConfig(config);
